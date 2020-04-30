e4458b031e052a4c947c63b898cb4aca2a5657ea
Onur Temiz <onurmain@gmail.com>
2020-04-30 15:05:15 +0300

    Added Build

---
+++ build/precache-manifest.16c62c425ab035bd1b58ed1ba81f1060.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d49262bc95d9b190cfc966e75054c26d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2c4cde7b06cfd11ac59",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8916a997227370ab0233",
+    "url": "/static/js/2.99c54e76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.99c54e76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2c4cde7b06cfd11ac59",
+    "url": "/static/js/main.9b20f93f.chunk.js"
+  },
+  {
+    "revision": "bccee6d650832870a352",
+    "url": "/static/js/runtime-main.5401d62a.js"
+  }
+]);
